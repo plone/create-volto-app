61c8cd6c98dc458eef8e563c7ea20dc6cae57c90
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-10-16 16:21:59 +0200

    Move folder to Plone only

--- cypress/integration/content.js
+++ cypress/integration/content.js
@@ -1,132 +1,122 @@
-context('Actions', () => {
+context("Actions", () => {
   beforeEach(() => {
     cy.autologin();
   });
-  it('As a site administrator I can add a page', function() {
-    cy.visit('/');
-    cy.get('#toolbar-add').click();
-    cy.get('#toolbar-add-document').click();
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
-      .type('This is a page')
-      .get('.documentFirstHeading span[data-text]')
-      .contains('This is a page');
+  it("As a site administrator I can add a page", function() {
+    cy.visit("/");
+    cy.get("#toolbar-add").click();
+    cy.get("#toolbar-add-document").click();
+    cy.get(".documentFirstHeading > .public-DraftStyleDefault-block")
+      .type("This is a page")
+      .get(".documentFirstHeading span[data-text]")
+      .contains("This is a page");
 
-    cy.get('#toolbar-save').click();
-    if (Cypress.env('API') === 'plone') {
-      cy.get('.navigation .item.active').should('have.text', 'This is a page');
+    cy.get("#toolbar-save").click();
+    if (Cypress.env("API") === "plone") {
+      cy.get(".navigation .item.active").should("have.text", "This is a page");
     } else {
-      cy.contains('This is a page');
+      cy.contains("This is a page");
     }
   });
-  it('As a site administrator I can add a page with text', function() {
-    cy.visit('/');
-    cy.get('#toolbar-add').click();
-    cy.get('#toolbar-add-document').click();
-    cy.get('.documentFirstHeading > .public-DraftStyleDefault-block')
-      .type('This is a page')
-      .get('.documentFirstHeading span[data-text]')
-      .contains('This is a page');
-    cy.get('.tile.inner.text .public-DraftEditor-content')
-      .type('This is the text.')
-      .get('span[data-text]')
-      .contains('This is the text');
+  it("As a site administrator I can add a page with text", function() {
+    cy.visit("/");
+    cy.get("#toolbar-add").click();
+    cy.get("#toolbar-add-document").click();
+    cy.get(".documentFirstHeading > .public-DraftStyleDefault-block")
+      .type("This is a page")
+      .get(".documentFirstHeading span[data-text]")
+      .contains("This is a page");
+    cy.get(".tile.inner.text .public-DraftEditor-content")
+      .type("This is the text.")
+      .get("span[data-text]")
+      .contains("This is the text");
 
-    cy.get('#toolbar-save').click();
-    if (Cypress.env('API') === 'plone') {
-      cy.get('.navigation .item.active').should('have.text', 'This is a page');
+    cy.get("#toolbar-save").click();
+    if (Cypress.env("API") === "plone") {
+      cy.get(".navigation .item.active").should("have.text", "This is a page");
     } else {
-      cy.contains('This is a page');
-      cy.contains('This is the text');
-    }
-  });
-  it('As a site administrator I can add a folder', function() {
-    cy.visit('/');
-    cy.get('#toolbar-add').click();
-    cy.get('#toolbar-add-folder').click();
-
-    if (Cypress.env('API') === 'guillotina') {
-      cy.get('.formtabs.menu')
-        .contains('default')
-        .click();
-    }
-
-    cy.get('input[name="title"]')
-      .type('This is a folder')
-      .should('have.value', 'This is a folder');
-
-    cy.get('#toolbar-save').click();
-
-    if (Cypress.env('API') === 'plone') {
-      cy.get('.navigation .item.active').should(
-        'have.text',
-        'This is a folder',
-      );
-    } else {
-      cy.contains('This is a folder');
+      cy.contains("This is a page");
+      cy.contains("This is the text");
     }
   });
 
   // Plone only tests
-  if (Cypress.env('API') === 'plone') {
-    it('As a site administrator I can add a news item', function() {
-      cy.visit('/');
-      cy.get('#toolbar-add').click();
-      cy.get('#toolbar-add-news-item').click();
-      cy.get('input[name="title"]')
-        .type('This is a news item')
-        .should('have.value', 'This is a news item');
+  if (Cypress.env("API") === "plone") {
+    it("As a site administrator I can add a folder", function() {
+      cy.visit("/");
+      cy.get("#toolbar-add").click();
+      cy.get("#toolbar-add-folder").click();
 
-      cy.get('#toolbar-save').click();
-      cy.get('.navigation .item.active').should(
-        'have.text',
-        'This is a news item',
+      cy.get('input[name="title"]')
+        .type("This is a folder")
+        .should("have.value", "This is a folder");
+
+      cy.get("#toolbar-save").click();
+
+      cy.get(".navigation .item.active").should(
+        "have.text",
+        "This is a folder"
       );
     });
-    it('As a site administrator I can add a file', function() {
-      cy.visit('/');
-      cy.get('#toolbar-add').click();
-      cy.get('#toolbar-add-file').click();
+    it("As a site administrator I can add a news item", function() {
+      cy.visit("/");
+      cy.get("#toolbar-add").click();
+      cy.get("#toolbar-add-news-item").click();
       cy.get('input[name="title"]')
-        .type('This is a file')
-        .should('have.value', 'This is a file');
+        .type("This is a news item")
+        .should("have.value", "This is a news item");
 
-      cy.fixture('file.pdf', 'base64').then(fileContent => {
-        cy.get('#field-file').upload(
-          { fileContent, fileName: 'file.pdf', mimeType: 'application/pdf' },
-          { subjectType: 'input' },
-        );
-        cy.get('#field-file')
-          .parent()
-          .parent()
-          .contains('file.pdf');
-      });
-
-      cy.get('#toolbar-save').click();
-      cy.visit('/contents');
-      cy.contains('This is a file');
+      cy.get("#toolbar-save").click();
+      cy.get(".navigation .item.active").should(
+        "have.text",
+        "This is a news item"
+      );
     });
-    it('As a site administrator I can add an image', function() {
-      cy.visit('/');
-      cy.get('#toolbar-add').click();
-      cy.get('#toolbar-add-image').click();
+    it("As a site administrator I can add a file", function() {
+      cy.visit("/");
+      cy.get("#toolbar-add").click();
+      cy.get("#toolbar-add-file").click();
       cy.get('input[name="title"]')
-        .type('This is an image')
-        .should('have.value', 'This is an image');
+        .type("This is a file")
+        .should("have.value", "This is a file");
 
-      cy.fixture('image.png', 'base64').then(fileContent => {
-        cy.get('#field-image').upload(
-          { fileContent, fileName: 'image.png', mimeType: 'image/png' },
-          { subjectType: 'input' },
+      cy.fixture("file.pdf", "base64").then(fileContent => {
+        cy.get("#field-file").upload(
+          { fileContent, fileName: "file.pdf", mimeType: "application/pdf" },
+          { subjectType: "input" }
         );
-        cy.get('#field-image')
+        cy.get("#field-file")
           .parent()
           .parent()
-          .contains('image.png');
+          .contains("file.pdf");
       });
 
-      cy.get('#toolbar-save').click();
-      cy.visit('/contents');
-      cy.contains('This is an image');
+      cy.get("#toolbar-save").click();
+      cy.visit("/contents");
+      cy.contains("This is a file");
+    });
+    it("As a site administrator I can add an image", function() {
+      cy.visit("/");
+      cy.get("#toolbar-add").click();
+      cy.get("#toolbar-add-image").click();
+      cy.get('input[name="title"]')
+        .type("This is an image")
+        .should("have.value", "This is an image");
+
+      cy.fixture("image.png", "base64").then(fileContent => {
+        cy.get("#field-image").upload(
+          { fileContent, fileName: "image.png", mimeType: "image/png" },
+          { subjectType: "input" }
+        );
+        cy.get("#field-image")
+          .parent()
+          .parent()
+          .contains("image.png");
+      });
+
+      cy.get("#toolbar-save").click();
+      cy.visit("/contents");
+      cy.contains("This is an image");
     });
   }
 });
