9721ee328f00e812df0f394d17d1f11088c7c072
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-10-16 20:58:41 +0200

    Revert "Add 2 retries"
    This reverts commit 18f8f22e53ee9bf9495d37f62415cc063fe6f492.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,22 +1,21 @@
-import xmlrpc from "xmlrpc";
-import "cypress-file-upload";
-import "cypress-plugin-retries";
-import "./commands";
-import { setupGuillotina, tearDownGuillotina } from "./guillotina";
+import xmlrpc from 'xmlrpc';
+import 'cypress-file-upload';
+import './commands';
+import { setupGuillotina, tearDownGuillotina } from './guillotina';
 
 beforeEach(function() {
-  cy.log("Setting up API fixture");
-  if (Cypress.env("API") === "plone") {
-    cy.exec("yarn cy:test:fixture:setup");
+  cy.log('Setting up API fixture');
+  if (Cypress.env('API') === 'plone') {
+    cy.exec('yarn cy:test:fixture:setup');
   } else {
     setupGuillotina();
   }
 });
 
 afterEach(function() {
-  cy.log("Tearing down API fixture");
-  if (Cypress.env("API") === "plone") {
-    cy.exec("yarn cy:test:fixture:teardown");
+  cy.log('Tearing down API fixture');
+  if (Cypress.env('API') === 'plone') {
+    cy.exec('yarn cy:test:fixture:teardown');
   } else {
     cy.clearCookies();
     tearDownGuillotina();
