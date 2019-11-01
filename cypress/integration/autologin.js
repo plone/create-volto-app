8ec0bd2235ca9d9a2e5c7a5c1690d018d90d4b50
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-11-01 11:36:35 +0100

    Fix g tests

--- cypress/integration/autologin.js
+++ cypress/integration/autologin.js
@@ -1,22 +1,26 @@
-context('Actions', () => {
-  it('As a site administrator I can add a page', function() {
-    let api_url;
-    if (Cypress.env('API') === 'guillotina') {
-      api_url = 'http://localhost:8081/db/container';
+describe("Autologin Tests", () => {
+  it("As a site administrator I can add a page", function() {
+    let api_url, user, password;
+    if (Cypress.env("API") === "guillotina") {
+      api_url = "http://localhost:8081/db/container";
+      user = "root";
+      password = "root";
     } else {
-      api_url = 'http://localhost:55001/plone';
+      api_url = "http://localhost:55001/plone";
+      user = "admin";
+      password = "secret";
     }
 
     cy.request({
-      method: 'POST',
+      method: "POST",
       url: `${api_url}/@login`,
-      headers: { Accept: 'application/json' },
-      body: { login: 'admin', password: 'secret' },
-    }).then(response => cy.setCookie('auth_token', response.body.token));
+      headers: { Accept: "application/json" },
+      body: { login: user, password: password }
+    }).then(response => cy.setCookie("auth_token", response.body.token));
 
-    cy.visit('/');
+    cy.visit("/");
 
-    cy.get('#toolbar-personal').click();
-    cy.get('#toolbar-logout').contains('Log out');
+    cy.get("#toolbar-personal").click();
+    cy.get("#toolbar-logout");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,19 @@
-Cypress.Commands.add('autologin', () => {
-  let api_url;
-  if (Cypress.env('API') === 'guillotina') {
-    api_url = 'http://localhost:8081/db/container';
+Cypress.Commands.add("autologin", () => {
+  let api_url, user, password;
+  if (Cypress.env("API") === "guillotina") {
+    api_url = "http://localhost:8081/db/container";
+    user = "root";
+    password = "root";
   } else {
-    api_url = 'http://localhost:55001/plone';
+    api_url = "http://localhost:55001/plone";
+    user = "admin";
+    password = "secret";
   }
 
   cy.request({
-    method: 'POST',
+    method: "POST",
     url: `${api_url}/@login`,
-    headers: { Accept: 'application/json' },
-    body: { login: 'admin', password: 'secret' },
-  }).then(response => cy.setCookie('auth_token', response.body.token));
+    headers: { Accept: "application/json" },
+    body: { login: user, password: password }
+  }).then(response => cy.setCookie("auth_token", response.body.token));
 });
